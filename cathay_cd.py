ed487adcbb97e63d4a462f07ef90fde934744ccd
lynden.huang <lynden.huang@gmail.com>
2020-09-17 17:24:24 +0800

    cathay flow update: damage_merge, summary

--- cathay_cd.py
+++ cathay_cd.py
@@ -2,6 +2,7 @@
 Cathay Car Damage Flow
 """
 import os
+import psutil
 import sys
 import datetime
 import time
@@ -9,8 +10,8 @@ import copy
 import math
 import logging
 import numpy as np
-import random
 import cv2
+import json
 
 import torch
 import torchvision
@@ -25,8 +26,9 @@ from classification import models
 
 from distutils.version import LooseVersion
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)  # logging.DEBUG
 
+IMGDICT = {}
 
 def gray_3_ch(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -125,12 +127,7 @@ class ImgSet(object):
         self.transforms = transforms
 
     def __getitem__(self, idx):
-        pimg = self.imgs[idx]
-        img = cv2.imread(pimg)
-        if img.shape[2] != 3:
-            img_org = gray_3_ch(img)
-        else:
-            img_org = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_org = self.imgs[idx]
         img_org = img_org.astype(np.uint8)
 
         if self.is_padding:
@@ -145,7 +142,7 @@ class ImgSet(object):
         if self.transforms is not None:
             img_torch = self.transforms(img_p_r.copy())
 
-        return img_torch, pimg
+        return img_torch
 
     def __len__(self):
         return len(self.imgs)
@@ -250,21 +247,26 @@ def mask_decode(image, result, classes, outputfolder, tag='c'):
     img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     _name = img_name+".jpg"
     path_org = os.path.join(outputfolder, _name)
+    if path_org not in IMGDICT.keys():
+        IMGDICT[path_org] = img_bgr
+
     cv2.imwrite(path_org, img_bgr)
+
     labels = result["labels"]
     boxes = result["boxes"]
     masks = result["masks"][:,:,:,0]
     logging.debug("<car seg.> mask:{}, boxes:{}".format(masks.shape, boxes.shape))
 
-    all_mask_img = np.zeros(img_bgr.shape)
+    if logging.getLogger().level == logging.DEBUG:
+        all_mask_img = np.zeros(img_bgr.shape)
+
     for _m in range(masks.shape[0]):
-        #pos = np.where(masks[_m,:,:]>0.5)
         lab = labels[_m]
 
         if tag == 'd':
             # damage area criteria
-            pos = np.where(masks[_m, :, :] > 0.5)
-            print(pos[0].shape[0], classes[lab])
+            pos = np.where(masks[_m,:,:] > 0.5)
+            logging.debug("damage pixels:{}, class:{}".format(pos[0].shape[0], classes[lab]))
             if pos[0].shape[0] < 4000 and classes[lab] in ['DS', 'DD', 'DC', 'DW']:
                 # the damage region is too small
                 continue
@@ -274,25 +276,25 @@ def mask_decode(image, result, classes, outputfolder, tag='c'):
         w, h = x2-x1, y2-y1
         logging.debug("<car box> (x1,y1):{}, (w,h):{}".format((x1,y1), (w,h)))
 
-        mask_img = np.zeros(img_bgr.shape)
-        pos_img = np.zeros((img_bgr.shape[0], img_bgr.shape[0]))
-        _name = img_name+"_" + tag + "_"+str(lab)+"_"+str(len(cls_list[lab]))+".jpg"
-
-        pos_img[:, :] = np.where(masks[_m, :, :] > 0.5, 1, 0)
-        for c in range(3):
-            all_mask_img[:,:,c] = np.where(masks[_m,:,:]>0.5, img_bgr[:,:,c], all_mask_img[:,:,c])
-            mask_img[:,:,c] = np.where(masks[_m,:,:]>0.5, img_bgr[:,:,c], 0)
-
+        _name = img_name + "_" + tag + "_" + str(lab) + "_" + str(len(cls_list[lab])) + ".jpg"
         path = os.path.join(outputfolder, _name)
-        #mask_img = mask_img[y1:y2,x1:x2,:]
-        #pos_img = pos_img[y1:y2,x1:x2,:]
-        cv2.imwrite(path, mask_img)
-        cls_list[lab].append((path_org, path, (x1,y1), (w,h), mask_img, pos_img))
+        if logging.getLogger().level == logging.DEBUG:
+            mask_img = np.zeros(img_bgr.shape)
+            for c in range(3):
+                all_mask_img[:,:,c] = np.where(masks[_m,:,:]>0.5, img_bgr[:,:,c], all_mask_img[:,:,c])
+                mask_img[:,:,c] = np.where(masks[_m,:,:]>0.5, img_bgr[:,:,c], 0)
+
+            mask_img = mask_img[y1:y1+h,x1:x1+w,:]
+            cv2.imwrite(path, mask_img)
+
+        cols, rows = np.where(masks[_m,:,:] > 0.5)
+        cls_list[lab].append((path_org, path, (x1,y1), (w,h), (cols,rows)))
 
     #logging.debug("predict list:{}".format(cls_list))
 
-    _name = img_name+"_" + tag + ".jpg"
-    cv2.imwrite(os.path.join(outputfolder, _name), all_mask_img)
+    if logging.getLogger().level == logging.DEBUG:
+        _name = img_name+"_" + tag + ".jpg"
+        cv2.imwrite(os.path.join(outputfolder, _name), all_mask_img)
 
     for i, c in enumerate(classes):
         cls_dict[c] = cls_list[i]
@@ -331,20 +333,48 @@ def damage_model(device):
 
     return mrcnn, CLASS
 
-def merge_car_damage_segs(cars, damages):
-    print(cars.keys(), damages.keys())
+def merge_car_damage_segs(cars, damages, outputfolder):
+    cls_dict = {}
     for _c in cars.keys():
+        cls_dict[_c] = []
         if _c in ["CC", "CP", "CL"]:
-            continue
+            cls_dict[_c] = cars[_c]
         else:
             for _cc in cars[_c]:
+                org_shape = IMGDICT[_cc[0]].shape
+                c_cols, c_rows = _cc[4]
+                c_pos = np.zeros((org_shape[0], org_shape[1]))
+                c_pos[c_cols, c_rows] = 1
+                logging.debug("<merge_car_damage_segs> org. shape:{}".format(org_shape))
+                loc_damages = {}
                 for _d in damages.keys():
                     for _dd in damages[_d]:
-                        print(_cc[1], _dd[1])
-                        iou_pos = _cc[5]*_dd[5]
+                        logging.debug("  car:{}, damage:{}".format(_cc[1], _dd[1]))
+                        d_cols, d_rows = _dd[4]
+                        d_pos = np.zeros((org_shape[0], org_shape[1]))
+                        d_pos[d_cols, d_rows] = 1
+                        iou_pos = c_pos*d_pos
                         iou = np.sum(np.sum(iou_pos, axis=0), axis=0)
-                        print(iou)
+                        logging.debug("  intersect iou:{}".format(iou))
+                        if iou > 0:
+                            cd_cols, cd_rows = np.where(iou_pos>0.5)
+                            c_pos = c_pos - iou_pos
+                            loc_damages[_d] = (cd_cols, cd_rows)
 
+                c_cols, c_rows = np.where(c_pos>0.5)
+                cls_dict[_c].append((_cc[0], _cc[1], _cc[2], _cc[3], (c_cols, c_rows), loc_damages))
+
+                if logging.getLogger().level == logging.DEBUG:
+                    img_bgr = IMGDICT[_cc[0]]
+                    updated_car_mask = np.zeros(org_shape)
+                    updated_name = os.path.basename(_cc[1]).split(".")[0]
+                    for c in range(3):
+                        updated_car_mask[:, :, c] = np.where(c_pos>0.5, img_bgr[:, :, c], 0)
+                    _name = updated_name + "_u.jpg"
+                    cv2.imwrite(os.path.join(outputfolder, _name), updated_car_mask)
+
+
+    return cls_dict
 
 @torch.no_grad()
 def car_segmentation(cases, folder):
@@ -383,7 +413,7 @@ def car_segmentation(cases, folder):
     header = '<car & damage seg.> Eval:'
     car_class_names = ['BG'] + car_classes
     damage_class_names = ['BG'] + damage_classes
-    car_class_dict = dict((k, []) for k in car_class_names)
+    car_return_dict = dict((k, []) for k in car_class_names)
     for images, targets in metric_logger.log_every(dbloader, dbloader.batch_size, header):
         images = list(img.to(device) for img in images)
         torch.cuda.synchronize()
@@ -392,7 +422,7 @@ def car_segmentation(cases, folder):
         c_outputs = [{k: v.to(device) for k, v in t.items()} for t in c_outputs]
         d_outputs = [{k: v.to(device) for k, v in t.items()} for t in d_outputs]
         #print(len(c_outputs), len(d_outputs))
-        car_mask_dict, damage_mask_dict = None, None
+        car_mask_dict, damage_mask_dict, car_damage_dict = None, None, None
         for _i, c in enumerate(c_outputs):
             logging.debug("car seg. target keys:{}".format(targets[_i].keys()))
             d = d_outputs[_i]
@@ -410,16 +440,16 @@ def car_segmentation(cases, folder):
                 car_mask_dict = mask_decode(images[_i], c_results, car_class_names, TMPFOLDER, tag='c')
                 major_car(car_mask_dict)
 
-                for _k in car_mask_dict.keys():
-                    car_class_dict[_k] += car_mask_dict[_k]
-
             if car_mask_dict is not None and damage_mask_dict is not None:
                 logging.debug("merge car & damage segmentations")
-                merge_car_damage_segs(car_mask_dict, damage_mask_dict)
+                car_damage_dict = merge_car_damage_segs(car_mask_dict, damage_mask_dict, TMPFOLDER)
 
+            if car_damage_dict is not None:
+                for _k in car_damage_dict.keys():
+                    car_return_dict[_k] += car_damage_dict[_k]
 
     #logging.debug("case seg. results:{}".format(class_dict))
-    return car_class_dict
+    return car_return_dict
 
 def case_division(seg_dict):
     case_dict = {"plates":[], "logos":[], "colors":[], "damages":[]}
@@ -439,20 +469,29 @@ def plate_detection(case_list):
     pass
 
 def color_detection(case_list):
-    # (path_org, path, (x1,y1), (w,h), part_img, pos_img)
+    # (path_org, path, (x1,y1), (w,h), (cols, rows))
     # received color seq: BGR
     #logging.debug("colors: {}".format(case_list))
     grid_size = 10
     colors = {}
     for c in case_list:
-        logging.debug("path:{}, (w,h):{}, mask_img:{}, pos_img:{}".format(c[1], c[3], c[4].shape, c[5].shape))
+        #logging.debug("path:{}, (w,h):{}, mask_img:{}, pos_img:{}".format(c[1], c[3], c[4].shape, c[5].shape))
+        path_org = c[0]
+        img_org = IMGDICT[path_org]
+        path = c[1]
         x1, y1 = c[2]
         w, h = c[3]
+        cols_org, rows_org = c[4]
         x = np.linspace(0, w, grid_size+1, dtype=np.int32)
         y = np.linspace(0, h, grid_size+1, dtype=np.int32)
-        #print(x, y)
-        mask_img = c[4][y1:y1 + h, x1:x1 + w, :]
-        pos_mask = c[5][y1:y1+h, x1:x1+w]
+        cols = cols_org - y1
+        rows = rows_org - x1
+        logging.debug("{},{},{},{},{}".format(path,(x1,y1),(w,h),cols.shape, rows.shape))
+
+        mask_img = np.zeros((h,w,3))
+        pos_mask = np.zeros((h,w))
+        mask_img[cols,rows,:] = img_org[cols_org,rows_org,:]
+        pos_mask[cols,rows] = 1
         for gx in range(grid_size):
             _x1, _x2 = x[gx], x[gx + 1]
             for gy in range(grid_size):
@@ -462,6 +501,8 @@ def color_detection(case_list):
                 if avg > 0:
                     avg = float(1.0/float(avg))
                     color_bgr_avg = np.array(color_bgr*avg, dtype=np.int32)
+                    #print(avg, color_bgr_avg)
+                    # input to rgb
                     cname = cathay_utils.closest_colour((color_bgr_avg[2],color_bgr_avg[1],color_bgr_avg[0]))
                     if cname in colors.keys():
                         colors[cname] += 1
@@ -477,7 +518,8 @@ def color_detection(case_list):
     else:
         colorName = "None"
 
-    logging.debug("color name: {}".format(colorName))
+    logging.info("<color detection> color detection: {}".format(colorName))
+    return colorName
 
 
 @torch.no_grad()
@@ -497,9 +539,24 @@ def logo_detection(case_list):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     torch.multiprocessing.freeze_support()
 
-    cases = [i[0] for i in case_list]
-    logging.debug("<logo detection> cases:{}".format(cases))
+    cases = []
+    for i in case_list:
+        img_bgr = IMGDICT[i[0]]
+        x1, y1 = i[2]
+        w, h = i[3]
+        cols_org, rows_org = i[4]
+        logo_img_bgr = np.zeros((h,w,3))
+        logo_img_rgb = np.zeros((h, w, 3))
+        cols = cols_org - y1
+        rows = rows_org - x1
+        logo_img_bgr[cols,rows,:] = img_bgr[cols_org,rows_org,:]
+        logo_img_rgb[:,:,0] = logo_img_bgr[:,:,2]
+        logo_img_rgb[:,:,1] = logo_img_bgr[:,:,1]
+        logo_img_rgb[:,:,2] = logo_img_bgr[:,:,0]
+        logging.debug("logo image shape:{}".format(logo_img_rgb.shape))
+        cases.append(logo_img_rgb)
 
+    logging.debug("<logo detection> cases:{}".format(cases))
     transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
     imgset = ImgSet(cases,
                     resize_img=[True, DIM, DIM],
@@ -518,7 +575,7 @@ def logo_detection(case_list):
     metric_logger = obj_utils.MetricLogger(delimiter="  ")
     header = '<logo> Eval:'
     results = []
-    for images, targets in metric_logger.log_every(dbloader, dbloader.batch_size, header):
+    for images in metric_logger.log_every(dbloader, dbloader.batch_size, header):
         images = images.to(device)
         torch.cuda.synchronize()
         outputs = resnet(images)
@@ -527,12 +584,33 @@ def logo_detection(case_list):
         results.append(max(set(preds), key=lambda x:preds.count(x)))
         #print(preds, type(preds))
 
-    max_pred_logo = CLASSES[max(set(results), key=lambda x:results.count(x))]
-    logging.debug("<logo> predicted logo:{}".format(max_pred_logo))
+    if len(results) > 0:
+        max_pred_logo = CLASSES[max(set(results), key=lambda x:results.count(x))]
+    else:
+        max_pred_logo = "None"
+    logging.info("<logo detection> predicted logo: {}".format(max_pred_logo))
     return max_pred_logo
 
-def summary():
-    pass
+def summary(seg_dict, logo, color, outputfolder):
+    name = "summary.json"
+    path = os.path.join(outputfolder, name)
+    data = {"images":len(IMGDICT), "logo":logo, "color":color, "cars":[]}
+    for seg in seg_dict.keys():
+        if seg in ['BG']:
+            continue
+        #print(seg_dict[seg], len(seg_dict[seg]))
+        for _c in seg_dict[seg]:
+            part = {"label": seg, "image_path":_c[0], "points": [], "damages": []}
+            if len(_c) < 6:
+                pass
+            else:              # including damages
+                pass
+
+            data["cars"].append(part)
+
+    json_data = json.dumps(data, indent=4, separators=(',', ': '))
+    with open(path, 'w') as fid:
+        fid.write(json_data)
 
 
 if __name__ == '__main__':
@@ -548,6 +626,8 @@ if __name__ == '__main__':
         if "tmp" in folder:
             continue
         for f in fs:
+            if "json" in f:
+                continue
             fpath = cathay_utils.path_join(folder, f)
             case_files.append(fpath)
 
@@ -555,20 +635,29 @@ if __name__ == '__main__':
 
     start_time = time.time()
     seg_dict = car_segmentation(case_files, args.case_path)
-    seg_time = time.time()
-    seg_time_str = str(datetime.timedelta(seconds=int(seg_time-start_time)))
+    seg_time_str = str(datetime.timedelta(seconds=int(time.time()-start_time)))
     logging.info("Car Segmentation Time: {}".format(seg_time_str))
+    logging.info("IMG Dataset:")
+    for i in IMGDICT.keys():
+        logging.info("key:{}, shape:{}".format(i, IMGDICT[i].shape))
 
     case_dict = case_division(seg_dict)
 
     plate_detection(case_dict["plates"])
 
-    color_detection(case_dict["colors"])
-    seg_time_str = str(datetime.timedelta(seconds=int(time.time()-seg_time)))
+    cur_tim = time.time()
+    color_name = color_detection(case_dict["colors"])
+    seg_time_str = str(datetime.timedelta(seconds=int(time.time()-cur_tim)))
     logging.info("Color Prediction Time: {}".format(seg_time_str))
 
-    #logo_detection(case_dict["logos"])
+    cur_time = time.time()
+    logo = logo_detection(case_dict["logos"])
+    logo_time_str = str(datetime.timedelta(seconds=int(time.time()-cur_time)))
+    logging.info("Logo Prediction Time: {}".format(logo_time_str))
 
-    summary()
+    summary(seg_dict, logo, color_name, outputfolder=args.case_path)
 
-    logging.info("Total Time:{}".format(time.time()-start_time))
+    logging.info("Total Time: {} s".format(time.time()-start_time))
+    process = psutil.Process(os.getpid())
+    MB = 1024*1024
+    logging.info("Memory Usage: {} MB".format(process.memory_info().rss/MB))
