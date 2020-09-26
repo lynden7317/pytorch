ed8e9be329ce37591ef8faa4312abef8d603f958
lynden.huang <lynden.huang@gmail.com>
2020-09-26 10:28:16 +0800

    refine Mask head layers

--- obj_detection/mask_rcnn.py
+++ obj_detection/mask_rcnn.py
@@ -1,4 +1,7 @@
+from collections import OrderedDict
+
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 
 from torchvision.models.detection.mask_rcnn import MaskRCNN as MRCNN
@@ -41,14 +44,68 @@ def MaskRCNN(backbone='default',
                                                     output_size=7,
                                                     sampling_ratio=2)
 
+    # output_size=14
     mask_roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
-                                                         output_size=14,
+                                                         output_size=28,
                                                          sampling_ratio=2)
+    mask_head = None
+    out_chs = backboneNet.out_channels
+    mask_dilation = 1
+    mask_head = MaskRCNNHeads(out_chs, mask_dilation)
 
     model = MRCNN(backboneNet,
                   num_classes=num_classes,
                   rpn_anchor_generator=anchor_generator,
                   box_roi_pool=roi_pooler,
-                  mask_roi_pool=mask_roi_pooler)
+                  mask_roi_pool=mask_roi_pooler,
+                  mask_head=mask_head)
 
     return model
+
+class MaskRCNNHeads(nn.Module):
+    def __init__(self, in_channels, dilation):
+        super(MaskRCNNHeads, self).__init__()
+
+        next_feature = in_channels
+        self.mask_fcn1 = nn.Conv2d(next_feature, 256, kernel_size=3,
+                                   stride=1, padding=dilation, dilation=dilation)
+        self.mask_fcn2 = nn.Conv2d(256, 256, kernel_size=3,
+                                   stride=1, padding=dilation, dilation=dilation)
+        self.mask_fcn3 = nn.Conv2d(256, 256, kernel_size=3,
+                                   stride=1, padding=dilation, dilation=dilation)
+        self.mask_fcn4 = nn.Conv2d(256, 256, kernel_size=3,
+                                   stride=1, padding=dilation, dilation=dilation)
+        self.mask_fcn5_t = nn.ConvTranspose2d(256, 256, 2, 2, 0)
+        self.mask_fcn6_t = nn.ConvTranspose2d(256, 256, 2, 2, 0)
+
+        for name, param in self.named_parameters():
+            if "weight" in name:
+                nn.init.kaiming_normal_(param, mode="fan_out", nonlinearity="relu")
+
+    def forward(self, x):
+        #print("before:{}".format(x.shape))
+        branch1 = F.relu(self.mask_fcn1(x))
+        x = F.max_pool2d(self.mask_fcn1(x), (2,2))
+        branch2 = F.relu(self.mask_fcn2(x))
+        x = F.max_pool2d(self.mask_fcn2(x), (2,2))
+        #print(x.shape)
+        x = self.mask_fcn5_t(x)
+        #print(x.shape)
+        x = F.relu(self.mask_fcn3(x+branch2))
+        x = self.mask_fcn6_t(x)
+        #print(x.shape)
+        x = F.relu(self.mask_fcn4(x+branch1))
+        #print("after:{}".format(x.shape))
+        return x
+
+
+"""
+CLASSES = 15
+mrcnn = MaskRCNN(backbone='resnet50',
+                 anchor_ratios=(0.33, 0.5, 1, 2, 3),
+                 num_classes=1 + CLASSES)
+
+print(mrcnn)
+print(list(mrcnn.children()))
+print(mrcnn.roi_heads.mask_head)
+"""
